4477b78caf6444a385833dc2da7f930a0da52066
KhadaRoss <WidowMine7525>
2019-06-07 10:10:23 +0200

    Remove auto generated copyright in login and logout controllers

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -1,19 +1,4 @@
 <?php
-/**
- * @copyright Copyright (c) rexx systems GmbH
- *
- * @link https://www.rexx-systems.com
- *
- * This software is protected by copyright.
- *
- * It is not permitted to copy, present, send, lease and / or lend the website
- * or individual parts thereof without the consent of the copyright holder.
- *
- * Contravention of this law will result in proceedings under criminal
- * or civil law.
- *
- * All rights reserved.
- */
 
 namespace controllers;
 
